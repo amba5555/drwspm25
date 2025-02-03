d99ed8f4dbc6359495ae90176d180aa66c198ea3
amba5555 <mcafeegood@gmail.com>
2025-02-04 01:00:27 +0700

    new date time direct from column A, add thai text

--- pm25monitor/views.py
+++ pm25monitor/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render
 
-GOOGLE_SCRIPT_URL = 'https://script.googleusercontent.com/macros/echo?user_content_key=ogFERX1HH3F6EIzu_JIdXg28akMlQWg-TMBPrBgQtyDQ76kx1NCyIAT7dlxT3DxCbtCF0cWn3tv2yd6ZWR6ksgWlpkFR2-r3m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJY3Gg1SgdkFV1dxD6V36nin0xcZKRaadKAWz44YP5FD9GjcVZRQccOn_xDcPFEweWFjVAWOBZRRoxM84HFoUQrouxKkSt0DNw&lib=MwMPtSVnqjpV6XcwrtYJ1uh9RNx0BfzFA'
+GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwzdVmaCSMkEcwmrA70U4IFKKY2GX01OPJIUXs_Eye-zkG-1UYbCglbk8LmCXIyPAneEg/exec'
 
 def index(request):  # <-- This function must exist
     return render(request, 'pm25monitor/index.html')
@@ -23,7 +23,8 @@ def get_pm25(request):
             
         return JsonResponse({
             'pm25': data.get('pm25', 'N/A'),
-            'timestamp': data.get('timestamp'),
+            'date': data.get('date', 'N/A'),
+            'time': data.get('time', 'N/A'),
             'avg_pm25': data.get('avg_pm25', 'N/A')
         })
     except Exception as e:
